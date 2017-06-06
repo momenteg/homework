3e099076fceb55061857a9f499220df396731d6e
momenteg <momenteg@gmail.com>
2017-06-06 18:31:56 +0200

    moving to more recent tools

--- Week_1/Array_and_pointers.cpp
+++ Week_1/Array_and_pointers.cpp
@@ -7,9 +7,9 @@
 
 int main(){
 	
-	char * p = new char[1]; //pointer to a character
+	std::unique_ptr<char> p(new char(1)); //pointer to a character
 	int array_int[10] = {0,1,2,3,4,5,6,7,8,9}; //int array
-	int* p1 = array_int; //pointer to array_int
+	std::unique_ptr<int> p1(new int(10))//pointer to array_int
 	char* p2[] = {"FBYC", "Sfortuna", "Verme", "GazeboPenguins"};	//pointer to an array of character string
 	//pointer to a pointer to a character
 	const int pi=3.14;
