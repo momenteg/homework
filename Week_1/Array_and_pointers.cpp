f88ebb31c5a8da6f8006e55a82013a634e04f0fb
momenteg <momenteg@gmail.com>
2017-06-06 23:08:35 +0200

    small update

--- Week_1/Array_and_pointers.cpp
+++ Week_1/Array_and_pointers.cpp
@@ -7,17 +7,16 @@
 
 int main(){
 	
-	std::unique_ptr<char> p(new char(1)); //pointer to a character
+	std::unique_ptr<char[]> p(new char(1)); //pointer to a character
 	int array_int[10] = {0,1,2,3,4,5,6,7,8,9}; //int array
-	std::unique_ptr<int> p1(new int(10))//pointer to array_int
-	char* p2[] = {"FBYC", "Sfortuna", "Verme", "GazeboPenguins"};	//pointer to an array of character string
+	std::unique_ptr<int> p1(new int(10));//pointer to array_int
+	std::unique_ptr<char[]> p2(new char(100));
+	*p2 = "FBYC";	//pointer to an array of character string
 	//pointer to a pointer to a character
 	const int pi=3.14;
-	//pointer to an integer costant
-	//cost pointer to an integer
-	
-	
-	
+	std::unique_ptr<int> pi_pointer(new int);
+	*pi_pointer = 3.14;
+	const std::unique_ptr<int> pi_pointer_bis(new int); 	
 	
 	return 0;
 }
