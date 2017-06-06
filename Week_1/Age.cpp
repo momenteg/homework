5a27a1ffde8e0d1026f61e259b36f8f7f0d34c98
momenteg <momenteg@gmail.com>
2017-06-06 22:27:10 +0200

    adding check in the input

--- Week_1/Age.cpp
+++ Week_1/Age.cpp
@@ -3,13 +3,26 @@
 #include <stdio.h>
 #include <iostream>
 #include <fstream>
+#include <stdlib.h>   
+#include <stdexcept>
+#include <string>
+
+
+inline void error(const std::string& s)
+{
+    throw std::runtime_error(s);
+}
 
 void ask_for_the_age(int &age){
 	std::cout << "What is your age?" << std::endl;
 	std::cin >> age;
-	std::cout << age << std::endl;
+	if(!std::cin.good()){
+	    error("Invalid input");
+	}else{
+	  std::cout << age << std::endl;	  
+	}
 }
-
+	
 void print_to_file(int age){
 	std::ofstream output;
 	output.open("Age_output.txt");
