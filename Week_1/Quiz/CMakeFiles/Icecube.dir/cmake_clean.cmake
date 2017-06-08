6b7083a13ef44f23410f7e41906825dcda9f0d5c
momenteg <momenteg@gmail.com>
2017-06-08 15:01:19 +0200

    Quiz

---
+++ Week_1/Quiz/CMakeFiles/Icecube.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Icecube.dir/Quiz.cpp.o"
+  "bin/Icecube.pdb"
+  "bin/Icecube"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Icecube.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Week_1/Quiz/CMakeFiles/Quiz.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Quiz.dir/Quiz.cpp.o"
+  "bin/Quiz.pdb"
+  "bin/Quiz"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Quiz.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
